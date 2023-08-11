88ab10d6c32e79d9cd74fd20b89609fa3802149a
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-08-10 23:12:18 -1000

    Working on an STS hillclimb.

--- maps_and_lists.exs
+++ maps_and_lists.exs
@@ -76,34 +76,45 @@ end
 
 # Instead, using module:
 defmodule Lists do
-  def reverse(list), do: do_reverse(list, [])
+  def reverse(list) when is_list(list), do: do_reverse(list, [])
   defp do_reverse([], acc), do: acc
   defp do_reverse([h|t], acc), do: do_reverse(t, [h | acc])
 
-  def length(list), do: do_length(list, 0)
+  def length(list) when is_list(list), do: do_length(list, 0)
   defp do_length([], acc), do: acc
   defp do_length([_ | tail], acc), do: do_length(tail, acc + 1)
 
-  # If the elements of a list can be converted to strings, this does so.
-  def str(list), do: str(list, &"#{&1}")
-
   # Takes a stringify function that converts the elements of the list
   # to strings. Needed if the element conversion is not straightforward.
-  def str(list, stringify), do: "[" <> str(list, "", stringify) <> "]"
+  # If no argument is given, just attempt a string conversion.
+  # This makes str into a str/1 and str/2.
+  def str(list, stringify \\ &"#{&1}") when is_list(list) and is_function(stringify, 1) do
+    "[" <> str(list, "", stringify) <> "]"
+  end
   defp str([], acc, _), do: String.trim_trailing(acc, ", ")
   defp str([h|t], acc, stringify) do
     str(t, acc <> stringify.(h) <> ", ", stringify)
   end
 
-  def zip(list1, list2), do: reverse(do_zip(list1, list2, []))
+  def zip(list1, list2) when is_list(list1) and is_list(list2) do
+    reverse(do_zip(list1, list2, []))
+  end
   defp do_zip([], _, acc), do: acc
   defp do_zip(_, [], acc), do: acc
   defp do_zip([x|xs], [y|ys], acc), do: do_zip(xs, ys, [[x,y]|acc])
 
-  def map_list(list, map_fn), do: reverse(do_map_list(list, [], map_fn))
-  defp do_map_list([], acc, _), do: acc
-  # defp do_map_list([h|t], acc, map_fn), do: do_map_list(t, "#{acc}#{map_fn.(h)} ", map_fn)
-  defp do_map_list([h|t], acc, map_fn), do: do_map_list(t, [map_fn.(h) | acc], map_fn)
+  # Partition a list into consecutive pairs of elements.
+  # If there is an odd number, drop the last.
+  def zip_in_pairs(list) when is_list(list), do: reverse(do_zip_in_pairs(list, []))
+  defp do_zip_in_pairs([], acc), do: acc
+  defp do_zip_in_pairs([_], acc), do: acc
+  defp do_zip_in_pairs([h1, h2 | t], acc), do: do_zip_in_pairs(t, [[h1, h2] | acc])
+
+  def map(list, map_fn) when is_list(list) and is_function(map_fn, 1) do
+    reverse(do_map(list, [], map_fn))
+  end
+  defp do_map([], acc, _), do: acc
+  defp do_map([h|t], acc, map_fn), do: do_map(t, [map_fn.(h) | acc], map_fn)
 end
 
 # Call with the lambda and with the module call.
@@ -112,7 +123,10 @@ IO.puts "#{Lists.str(new_list)} has length #{Lists.length(new_list)}"
 
 # Zip the two lists together and print.
 zipped = Lists.zip(new_list, list)
-str_zipped = Lists.map_list(zipped, &Lists.str/1)
+str_zipped = Lists.map(zipped, &Lists.str/1)
 IO.puts Lists.str(new_list) <> " and " <> Lists.str(list) <> " zipped are: " <> Lists.str(str_zipped)
+
+# Zip a list in pairs.
+IO.inspect Lists.zip_in_pairs([0, 2, 1, 3, 4, 6, 5, 7, 8])
 
 System.halt 0
